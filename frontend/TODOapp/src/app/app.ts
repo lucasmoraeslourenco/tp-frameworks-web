71ebda677ae2380d3593ef66d61bb88eacaccf70
lucasmoraeslourenco <126622295+lucasmoraeslourenco@users.noreply.github.com>
2026-04-22 19:49:29 -0300

    Update API URL in app.ts

--- frontend/TODOapp/src/app/app.ts
+++ frontend/TODOapp/src/app/app.ts
@@ -16,7 +16,7 @@ export class App implements OnInit {
   apiURL: string;
 
   constructor(private http: HttpClient) {
-    this.apiURL = 'https://tarefasapilucas252838.onrender.com';
+    this.apiURL = 'https://tarefasapijoaopedro252959lucasmoraes2528.onrender.com';
     this.READ_tarefas();
   }
 
